6c4ea2aa8e703919baeafd4edf4f1e23da7ad282
megumigachi <472014574@qq.com>
2023-10-11 08:29:06 -0700

    1

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -6,15 +6,33 @@
 // automated checks run by `make check_lab2`.
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
 bool TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    // if the segment contains syn
+    const TCPHeader header = seg.header();
+    if (header.syn) {
+        if (!_syn_received) {
+            _syn_received = true;
+            // init isn and ackno
+            _isn = header.seqno;
+            _ackno = header.seqno + 1;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    // if the segment contains fin
     return {};
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    if (!this->_syn_received) {
+        return {};
+    }
+    return _ackno;
+}
 
 size_t TCPReceiver::window_size() const { return {}; }
--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -20,12 +20,25 @@ class TCPReceiver {
     //! The maximum number of bytes we'll store.
     size_t _capacity;
 
+    bool _syn_received;
+    bool _fin_received;
+    WrappingInt32 _isn;
+    WrappingInt32 _ackno;
+    uint64_t _checking_point;
+
   public:
     //! \brief Construct a TCP receiver
     //!
     //! \param capacity the maximum number of bytes that the receiver will
     //!                 store in its buffers at any give time.
-    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) {}
+    TCPReceiver(const size_t capacity)
+        : _reassembler(capacity)
+        , _capacity(capacity)
+        , _syn_received(false)
+        , _fin_received(false)
+        , _isn(0)
+        , _ackno(0)
+        , _checking_point(0) {}
 
     //! \name Accessors to provide feedback to the remote TCPSender
     //!@{
